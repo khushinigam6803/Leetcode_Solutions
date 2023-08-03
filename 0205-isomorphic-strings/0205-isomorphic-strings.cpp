e0ccdd825bae2e7dcc3697b740e3b6ffdb09cbb5
khushinigam6803 <96129963+khushinigam6803@users.noreply.github.com>
2023-08-04 00:44:38 +0530

    Time: 1723 ms (5.68%), Space: 7.1 MB (25.65%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        if(s.length()!=t.length())
+        return false;
+        for(int i=0;i<s.length()-1;i++)
+        {
+            //i have s[i], t[i]
+            for(int j=i+1;j<s.length();j++)
+            {
+                if(s[i]==s[j]&&t[i]!=t[j])
+                return false;
+                if(s[i]!=s[j]&&t[i]==t[j])
+                return false;
+            }
+        }
+        
+        return true;
+    }
+};
