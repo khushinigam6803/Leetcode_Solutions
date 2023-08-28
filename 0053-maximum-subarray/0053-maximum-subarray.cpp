792651a6ccac1d59e6bb361205c188a9835d70df
khushinigam6803 <96129963+khushinigam6803@users.noreply.github.com>
2023-08-29 00:38:07 +0530

    Time: 73 ms (95.39%), Space: 67.7 MB (83.93%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,17 +1,15 @@
+
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
         int max=INT_MIN;
-        int temp=0;
+        int curr=0;
         for(int i=0;i<nums.size();i++)
         {
-            temp+=nums[i];
-            if(temp>max)
-                max=temp;
-            if(temp<0)
-                temp=0;
+            curr=std::max(nums[i], nums[i]+curr);
+            max=std::max(max, curr);
         }
         return max;
-        
+
     }
 };
